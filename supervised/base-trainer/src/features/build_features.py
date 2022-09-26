8acbf562ca696a871d096f5a187d20ba495aa82d
Nathaniel Egwu <nathaniel@aims.ac.za>
2022-09-26 18:49:26 +0000

    Updated supervised and unsupervised folders

---
+++ supervised/base-trainer/src/features/build_features.py
@@ -0,0 +1,14 @@
+""" Import data and prepare features """
+class Features:
+    """ Main features class """
+
+    def __init__(self, x_num, y_num) -> None:
+        """ Initialize the main class variables """
+        self.x_num = x_num
+        self.y_num = y_num
+
+    def build_features(self, num):
+        """ prepare data set for training """
+
+    def check_dataset_feats(self):
+        """ final features preparation prior to training """
